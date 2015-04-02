550e2ee70f26c41eae09bee7d80747552b3f409f
Adam Kochanowicz <ajkochanowicz@gmail.com>
2015-04-02 15:20:21 -0400

    Adding twitter auth keys

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401202509) do
+ActiveRecord::Schema.define(version: 20150402191416) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20150401202509) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "handle"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
