5550ce0256cc4ce4a69055fcd78cba3d2d85e7b6
Adam Kochanowicz <ajkochanowicz@gmail.com>
2015-04-06 18:09:31 -0400

    Adding score for Issue

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -1,3 +1,7 @@
 class IssueSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :created_at
+  attributes :id, :name, :description, :created_at, :score
+
+  def score
+    object.get_upvotes.size - object.get_downvotes.size
+  end
 end
