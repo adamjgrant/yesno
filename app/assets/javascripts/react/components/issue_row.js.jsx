aa847a3cca22830cc160203b73d697acd7ffe0a4
Adam Kochanowicz <ajkochanowicz@gmail.com>
2015-04-06 22:07:29 -0400

    Index page links to show page

--- app/assets/javascripts/react/components/issue_row.js.jsx
+++ app/assets/javascripts/react/components/issue_row.js.jsx
@@ -2,7 +2,9 @@ var IssueRow = React.createClass({
   render: function() {
     return (
       <article data-component="issue">
-        <h1>{this.props.issue.name}</h1>
+        <h1>
+          <a href={"/issues/" + this.props.issue.id}>{this.props.issue.name}</a>
+        </h1>
         <h2>{this.props.issue.description}</h2>
       </article>
     )
