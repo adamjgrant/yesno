d6f5d82908be0ef56c106f30d3f45526856237e6
Adam Grant <hello@adamgrant.me>
2015-08-08 07:32:31 -0700

    Nested comments now save successfully

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,8 +15,8 @@ class CommentsController < ApplicationController
   end
 
   def create
-    if params[:comment][:parent_id].to_i > 0
-      parent = Comment.find_by_id(params[:comment].delete(:parent_id))
+    if params[:parent_id].to_i > 0
+      parent = Comment.find_by_id(params[:parent_id])
       @comment = parent.children.build(comment_params)
     else
       @opinion = Opinion.find(params[:opinion_id])
@@ -25,12 +25,7 @@ class CommentsController < ApplicationController
     end
 
     if @comment.save
-      if @comment.opinion
-        render :text => "Comment added successfully", :status => 200
-      else
-        @comment = Comment.find_by_id(@comment.parent_id)
-        redirect_to @comment, :action => "new"
-      end
+      render :text => "Comment added successfully", :status => 200
       flash[:success] = "Comment added."
     end
   end
