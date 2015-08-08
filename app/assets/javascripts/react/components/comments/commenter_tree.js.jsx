5d60c48eb24ee2b518e497094392d3d77f978a62
Adam Grant <hello@adamgrant.me>
2015-08-07 17:10:06 -0700

    Fixing js formatting bug

--- app/assets/javascripts/react/components/comments/commenter_tree.js.jsx
+++ app/assets/javascripts/react/components/comments/commenter_tree.js.jsx
@@ -1,10 +1,10 @@
 var CommenterTree = React.createClass({
-  var comments = this.props.comments.map(function(comment) {
-    return (
-      <CommenterTreeComment comment={ comment } key={ "comment-" + comment.id } />
-    )
-  });
   render: function() {
+    var comments = this.props.comments.map(function(comment) {
+      return (
+        <CommenterTreeComment comment={ comment } key={ "comment-" + comment.id } />
+      )
+    });
     return (
       <div>
         { comments }
