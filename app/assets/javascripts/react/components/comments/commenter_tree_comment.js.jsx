c8da6e6c2d4487a6fd40981195fef92ec0671c16
Adam Grant <hello@adamgrant.me>
2015-08-07 18:34:03 -0700

    Reply link launches modal

--- app/assets/javascripts/react/components/comments/commenter_tree_comment.js.jsx
+++ app/assets/javascripts/react/components/comments/commenter_tree_comment.js.jsx
@@ -1,14 +1,36 @@
 var CommenterTreeComment = React.createClass({
+  getInitialState: function() {
+    return({
+      modalVisibility: { display: "none" }
+    });
+  },
+  showModal: function(e) {
+    var state = this.state;
+    state.modalVisibility = {
+      display: "block"
+    };
+    this.setState(state);
+    e.stopPropagation();
+  },
   reply: function(id) {
     return this.props.reply(id);
+  },
+  componentDidMount: function() {
+    k$.modal('.modal-' + this.props.comment.id);
   },
   render: function() {
     return (
       <div data-component="comment">
+        <div data-component="modal" className={"modal-" + this.props.comment.id } style={ this.state.modalVisibility }>
+          <CommenterComposer 
+            commentId={ this.props.comment.id }
+          />
+          <button onClick={ this.reply.bind(null, this.props.comment.id) }>Reply</button>
+        </div>
         <p>{ this.props.comment.body }</p>
         <footer>
           <p>
-            <a href="#" onClick={ this.reply.bind(null, this.props.comment.id) }>
+            <a href="#" onClick={ this.showModal }>
               Reply
             </a>
           </p>
