c056b54ea83f0063ae8adc6e932f69fa3b206967
Adam Grant <hello@adamgrant.me>
2015-08-12 15:25:54 -0700

    Destroying opinions destroys votes as well

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -11,4 +11,9 @@ class Opinion < ActiveRecord::Base
   def to_param
     [id, statement.slice(0, 40).parameterize].join("-")
   end
+
+  def destroy
+    @votes = Vote.where(votable_type: "Issue", votable_id: self.issue.id)
+    @votes.destroy_all
+  end
 end
