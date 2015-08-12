615f01f8df3cb46774b910ac089cfe42be0322e0
Adam Grant <hello@adamgrant.me>
2015-08-12 09:34:18 -0700

    Polled notifications now show up in popup

--- app/assets/javascripts/react/components/notification_badge.js.jsx
+++ app/assets/javascripts/react/components/notification_badge.js.jsx
@@ -4,8 +4,17 @@ var NotificationBadge = React.createClass({
       newNotifications: 0
     }
   },
-  getData: function() {
+  getData: function(showGrowls) {
     $YN.get("/notifications.json", function(data) {
+      if (showGrowls && this.state.newNotifications !== data.notifications) {
+        var notification = data.notifications[data.notifications.length - 1]
+        k$.growl({
+          title: "New notification",
+          text: "<p><a href=\"/notifications\">Go to your notifications</a></p>",
+          type: 'green',
+          delay: 2500
+        });
+      }
       this.setState({ newNotifications: data.notifications });
     }.bind(this));
   },
@@ -14,7 +23,7 @@ var NotificationBadge = React.createClass({
     var pollCount = 0,
         poll = setInterval(function() {
           pollCount++;
-          pollCount > 3 ? window.clearInterval(poll) : this.getData();
+          pollCount > 3 ? window.clearInterval(poll) : this.getData(true);
         }.bind(this), 3000);
   },
   render: function() {
