d7cb2a3f925ab3ff87ba91b7f3d9229d0f9f99c1
Adam Grant <hello@adamgrant.me>
2015-08-14 08:24:43 -0700

    Styling of issue page

--- app/assets/javascripts/react/components/opinions/opinion_table.js.jsx
+++ app/assets/javascripts/react/components/opinions/opinion_table.js.jsx
@@ -2,6 +2,7 @@ var OpinionTable = React.createClass({
   render: function() {
     return (
       <section data-component="opinion_table">
+        <h1>Opinions</h1>
         <div>
           <OpinionIndex 
             issue={this.props.issue} 
