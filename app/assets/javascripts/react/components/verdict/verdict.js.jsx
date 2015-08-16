c5adf4b68b79897efd3c2bc595a03f2a2d943cf2
Adam Grant <hello@adamgrant.me>
2015-08-16 01:18:46 -0700

    Adding all icons

--- app/assets/javascripts/react/components/verdict/verdict.js.jsx
+++ app/assets/javascripts/react/components/verdict/verdict.js.jsx
@@ -3,12 +3,17 @@ var Verdict = React.createClass({
     var score = this.props.score,
       response = this.props.response,
       yes_statement = this.props.yes,
-      no_statement = this.props.no;
+      no_statement = this.props.no,
+      icon = "question";
+
+    if (this.props.votes > 0) {
+      icon = (score === 0 ? "tie" : (score > 0 ? "thumbs-up" : "thumbs-down" ));
+    }
 
     return (
       <aside className={ (score == 0 ? '' : response.toLowerCase()) + " verdict " + this.props.className }>
         <Icon 
-          icon={ score === 0 ? "tie" : (score > 0 ? "thumbs-up" : "thumbs-down" ) }
+          icon={ icon }
         />
       </aside>
     )
