175f555eff8e3167673b7df07ccdf1971917c91b
Adam Grant <hello@adamgrant.me>
2015-08-16 23:30:15 -0700

    Trying to fix longtext errors

--- db/migrate/20150817062150_change_data_types.rb
+++ db/migrate/20150817062150_change_data_types.rb
@@ -1,6 +1,6 @@
 class ChangeDataTypes < ActiveRecord::Migration
   def change
-    change_column :opinions, :statement, :text
-    change_column :comments, :body, :text
+    change_column :opinions, :statement, :text, :limit => nil
+    change_column :comments, :body, :text, :limit => nil
   end
 end
--- db/migrate/20150817062433_convert_to_long_text.rb
+++
@@ -1,6 +0,0 @@
-class ConvertToLongText < ActiveRecord::Migration
-  def change
-    change_column :opinions, :statement, :longtext
-    change_column :comments, :body, :longtext
-  end
-end
